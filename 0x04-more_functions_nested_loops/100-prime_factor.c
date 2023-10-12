abff07168e0e0b6af526d93e809d7a86a3c272e8
Valritehachi <valritehachi@gmail.com>
2023-10-12 07:32:54 -0700

    edits

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,47 +1,40 @@
-#include "main.h"
-
 #include <stdio.h>
+#include <math.h>
 
 /**
-*print_to_98 - prints all natural numbers from n to 98
-*@n: The number to start printing from
-*Return: Always 0.
-*/
-
-void print_to_98(int n)
-
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
+	long int n;
+	long int max;
+	long int i;
 
-	if (n <= 98)
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
 	{
-	for (; n <= 98; n++)
+		max = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
 	}
-	else
-	{
-	printf("%d, ", n);
-	}
-	}
-	}
-	else
-	{
-	for (; n >= 98; n++)
-	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
-	else
-	{
-	printf("%d, ", n);
-	}
-	}
-	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
 }
+
