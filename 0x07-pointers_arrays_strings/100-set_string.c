adf30f38e998488daa680afe4d5663821dbee294
Valritehachi <valritehachi@gmail.com>
2023-10-23 05:21:16 -0700

    pointers and arrays

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+/**
+ * set_string - sets the value of a pointer to a new string
+ * @s: The pointer to a pointer to a string
+ * @to: The string to set the pointer to
+ */
+
+void set_string(char **s, char *to)
+
+{
+
+*s = to;
+
+}
