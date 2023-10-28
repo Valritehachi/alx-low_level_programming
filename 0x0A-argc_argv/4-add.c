833e5b99f01a1b3f8ad1f2d41720fa5f33d14752
Valritehachi <valritehachi@gmail.com>
2023-10-28 07:02:01 -0700

    edits

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
+#include <string.h>
+
 /**
-* main - main function
-* @argc: argument count
-* @argv: arguments
-* Return: dest.
-*/
+ * check_num - Check a string's contents
+ * @str: The string to check
+ *
+ * Return: 1 if string has numeric characters, 0 otherwise.
+ */
+
+int check_num(char *str)
+{
+unsigned int count;
+
+count = 0;
+while (count < strlen(str))
+{
+if (!isdigit(str[count]))
+{
+return (0);
+}
+count++;
+}
+return (1);
+}
+
+/**
+ * main - Entry point
+ * @argc: command-line arguments
+ * @argv: array of command-line argument.
+ * Return: 0 on success, 1 on error
+ */
+
 int main(int argc, char *argv[])
 {
-	int sum, num, i;
 
-	if (argc < 2)
-	{
-		puts("0\n");
-		/* return (0); */
-	}
-	sum = 0;
-	for (i = 1; i < argc; i++)
-	{
-		num = atoi(argv[i]);
-		if (num == 0)
-		{
-			puts("Error\n");
-			return (1);
-		}
-		sum = sum + num;
-	}
-	printf("%d\n", sum);
-	return (0);
+int str_to_int;
+int count;
+int sum = 0;
+
+count = 1;
+while (count < argc)
+{
+if (check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]);
+
+sum += str_to_int;
+}
+else 
+{
+printf("Error\n");
+return (1);
+}
+count++;
+}
+printf("%d/n", sum);
+return (0);
 }
