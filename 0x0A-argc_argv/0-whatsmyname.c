4d1774bc5ddc319c5acf7c8edaef037569533d69
Valritehachi <valritehachi@gmail.com>
2023-10-27 19:26:41 -0700

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* @argc: argument count
+* @argv: arguments
+* Return: dest.
+*/
+int main(int argc, char *argv[])
+{
+	puts(argv[0]);
+	return (argc);
+}
