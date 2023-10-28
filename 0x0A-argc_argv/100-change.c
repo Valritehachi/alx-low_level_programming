028b5a514f9d4d238d93f0085416e065fd2535f5
Valritehachi <valritehachi@gmail.com>
2023-10-28 07:34:37 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,47 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
 * main - main function
 * @argc: argument count
 * @argv: arguments
-* Return: dest.
+* Return: 0.
 */
+
 int main(int argc, char *argv[])
 {
-	int cents;
-	/* int places[5] = {25, 10, 5, 2, 1}; */
-	int coins;
+int num, cent, i;
 
-	if (argc != 2)
-	{
-		puts("Error\n");
-		return (1);
-	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
-	{
-		puts("0\n");
-		return (1);
-	}
-	coins = 0;
-	coins += cents / 25;
-	cents = cents % 25;
-	coins += cents / 10;
-	cents = cents % 10;
-	coins += cents / 5;
-	cents = cents % 5;
-	coins += cents / 2;
-	cents = cents % 2;
-	coins += cents;
-	/**
-	* for (i = 0; i < 5; i++)
-	* {
-	*	coins += cents / places[i];
-	*	cents = cents % places[i];
-	* }
-	*/
-	printf("%d\n", coins);
-	return (0);
+int coins[] = {25, 10, 5, 2, 1};
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
 }
-
+num = atoi(argv[1]);
+cent = 0;
+if (num < 0)
+{
+printf("0\n");
+return (0);
+}
+for (i = 0; i < 5 && num >= 0; i++)
+{
+while (num >= coins[i])
+{
+cent++;
+num -= coins[i];
+}
+}
+printf("%d\n", cent);
+return (0);
+}
