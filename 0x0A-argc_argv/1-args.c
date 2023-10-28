6ebe00a57f0760ccb385c10b19d2c3ca5432f489
Valritehachi <valritehachi@gmail.com>
2023-10-27 19:48:41 -0700

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - main function
+* @argc: argument count
+* @argv: arguments
+* Return: dest.
+*/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc);
+	return (0);
+}
