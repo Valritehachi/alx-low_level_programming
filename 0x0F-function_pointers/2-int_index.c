cfdde9973162c1136415ea0b70998edb7c81e86d
Valritehachi <valritehachi@gmail.com>
2023-11-08 01:26:57 -0800

    updates

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,18 +11,21 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+int a = 0;
 
-	if (size <= 0)
-	{
-		return (-1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-		{
-			return (i);
-		}
-	}
-	return (-1);
+if (size > 0)
+{
+if (array != NULL && cmp != NULL)
+{
+while (a < size)
+{
+if (cmp(array[a]))
+return (a);
+
+a++;
+}
+}
+}
+
+return (-1);
 }
