d898b82b13a52a355dba09b010d57b39541b18f5
Valritehachi <valritehachi@gmail.com>
2023-11-08 01:41:52 -0800

    edits

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "function_pointers.h"
 #include "3-calc.h"
+#include <string.h>
 /**
  * get_op_func - check if a number is equal to 98
  * @s: the integer to check
@@ -17,15 +18,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+int i = 0;
 
-	while (ops[i].op != NULL)
-	{
-		if ((ops[i].op[0] == s[0]) && s[1] == 0)
-		{
-			return (ops[i].f);
-		}
-		i++;
-	}
-	return (NULL);
+while (i < 5)
+{
+if (strcmp(s, ops[i].op) == 0)
+return (ops[i].f);
+
+i++;
+}
+
+return (0);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
  * main - Entry point of the program
@@ -11,29 +11,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char op;
-	int (*op_func)(int a, int b);
 
-	if (argc != 4)
-	{
-		puts("Error\n");
-		return (98);
-	}
-	num1 = atoi(argv[1]);
-	op = argv[2][0];
-	num2 = atoi(argv[3]);
-	if ((op == '/' || op == '%') && argv[2][1] == 0 && num2 == 0)
-	{
-		puts("Error\n");
-		return (100);
-	}
-	op_func = get_op_func(argv[2]);
-	if (op_func == NULL)
-	{
-		puts("Error\n");
-		return (99);
-	}
-	printf("%d\n", op_func(num1, num2));
-	return (0);
+int (*op)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+op = get_op_func(argv[2]);
+
+if (!op)
+{
+printf("Error\n");
+exit(99);
+}
+
+printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+return (0);
 }
