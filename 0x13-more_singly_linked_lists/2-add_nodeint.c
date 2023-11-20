2ccb7f2b29b15a9629e48cd2e8b79cf8a0bb7f59
Valritehachi <valritehachi@gmail.com>
2023-11-20 08:21:55 -0800

    Write a function that adds a new node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds new node
+ * @n: Integer value
+ * @head: Pointer
+ * Return: head.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+
+listint_t *new_node;
+
+new_node = malloc(sizeof(listint_t));
+
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+new_node->next = *head;
+
+*head = new_node;
+
+return (*head);
+}
