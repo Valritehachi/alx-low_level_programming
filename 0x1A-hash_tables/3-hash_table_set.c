d512e1910d95ce43ebaa4feb14f12bdce91faf60
Valritehachi <valritehachi@gmail.com>
2024-02-02 07:32:17 -0800

    others

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,33 +3,6 @@
 #include "hash_tables.h"
 
 /**
- * create_node - check the code
- * @key: ...
- * @value: ...
- * Return: Always EXIT_SUCCESS.
- */
-hash_node_t *create_node(char *key, const char *value)
-{
-	hash_node_t *node;
-
-	node = (hash_node_t *)calloc(1, sizeof(hash_node_t));
-	if (node == NULL)
-	{
-		return (NULL);
-	}
-	node->key = strdup(key);
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node);
-		return (NULL);
-	}
-	strcpy(node->value, value);
-
-	return (node);
-}
-
-/**
  * hash_table_set - check the code
  * @ht: ...
  * @key: ...
@@ -38,35 +11,43 @@ hash_node_t *create_node(char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
-	unsigned long hash_index;
-	hash_node_t *entry;
+	unsigned long int index = 0;
+	char *valuecopy, *keycopy;
+	hash_node_t  *bucket, *new_node;
 
-	if (key == NULL || *key == 0)
+	if (!ht || !key || !*key || !value)
+		return (0);
+
+	valuecopy = strdup(value);
+	if (!valuecopy)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[index];
+
+	while (bucket)
 	{
+		if (!strcmp(key, bucket->key))
+		{
+			free(bucket->value);
+			bucket->value = valuecopy;
+			return (1);
+		}
+		bucket = bucket->next;
+	}
+	new_node = calloc(1, sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(valuecopy);
 		return (0);
 	}
-
-	hash_index = key_index((unsigned char *)key, 1024);
-	entry = ht->array[hash_index];
-	if (entry == NULL)
-	{
-		entry = create_node((char *)key, value);
-		if (entry == NULL)
-		{
-			return (0);
-		}
-		ht->array[hash_index] = entry;
-	}
-	else
-	{
-		node = create_node((char *)key, value);
-		if (node == NULL)
-		{
-			return (0);
-		}
-		node->next = entry;
-		ht->array[hash_index]  = node;
-	}
+	keycopy = strdup(key);
+	if (!keycopy)
+		return (0);
+	new_node->key = keycopy;
+	new_node->value = valuecopy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
+
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -15,23 +15,18 @@ int main(void)
 	ht = hash_table_create(1024);
 	printf("Hash Table: \n");
 	hash_table_set(ht, "betty", "cool");
-	print_hash_table(ht);
 
 	hash_table_set(ht, "hetairas", "random 1");
 	hash_table_set(ht, "mentioner", "random 2");
-	print_hash_table(ht);
 
 	hash_table_set(ht, "heliotropes", "random 3");
 	hash_table_set(ht, "neurospora", "random 4");
-	print_hash_table(ht);
 
 	hash_table_set(ht, "stylist", "random 5");
 	hash_table_set(ht, "subgenera", "random 6");
-	print_hash_table(ht);
 
 	hash_table_set(ht, "dram", "random 7");
 	hash_table_set(ht, "vivency", "random 8");
-	print_hash_table(ht);
 
 	return (EXIT_SUCCESS);
 }
